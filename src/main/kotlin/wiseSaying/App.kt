d51313bb1e57e7080348848f1403dd50a5256aa5
sat7312 <sat7312@gmail.com>
2026-04-30 14:21:03 +0900

    10 SystemController, WiseSayingController 도입

--- src/main/kotlin/wiseSaying/App.kt
+++ src/main/kotlin/wiseSaying/App.kt
@@ -1,6 +1,9 @@
 package wiseSaying
 
-class App {
+class App(
+    val wiseSayingController: WiseSayingController = WiseSayingController(),
+    val systemController: SystemController = SystemController()
+) {
 
     fun run() {
         var lastId = 0
@@ -15,56 +18,10 @@ class App {
             val rq = Rq(input)
 
             when (input) {
-                "종료" -> {
-                    println("프로그램을 종료합니다.")
-                    break
-                }
-
-                "등록" -> {
-                    print("명언: ")
-                    val content = readln()
-                    print("작가: ")
-                    val author = readln()
-                    val id = ++lastId
-
-                    WiseSaying(lastId, content, author)
-                        .also {
-                            wiseSayings.add(it)
-                        }
-
-                    println("${id}번 명언이 등록되었습니다.")
-                }
-
-                "목록" -> {
-                    println("번호 / 작가 / 명언")
-                    println("-".repeat(30))
-                    wiseSayings.reversed().forEach {
-                        println("${it.id} / ${it.author} / ${it.content}\n")
-                    }
-                }
-
-                "삭제" -> {
-                    val id = rq.getParamAsInt("id", 0)
-
-                    if (id == 0) {
-                        println("id를 입력해주세요.")
-                        continue
-                    }
-
-                    wiseSayings.removeIf {
-                        it.id == id
-                    }
-
-                    val rst = wiseSayings
-                        .firstOrNull {
-                            it.id == id
-                        }
-                        ?.let {
-                            wiseSayings.remove(it)
-                        }
-
-                    println("${id}번 명언이 삭제되었습니다.")
-                }
+                "종료" -> systemController.exit()
+                "등록" -> wiseSayingController.write()
+                "목록" -> wiseSayingController.list()
+                "삭제" -> wiseSayingController.delete(rq)
             }
         }
     }
