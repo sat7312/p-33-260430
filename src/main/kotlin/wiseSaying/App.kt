f20b585658e7f55e1ea39b3bcfd6e162ffed8eb5
sat7312 <sat7312@gmail.com>
2026-04-30 11:20:02 +0900

    4 App 클래스 도입

---
+++ src/main/kotlin/wiseSaying/App.kt
@@ -0,0 +1,31 @@
+package wiseSaying
+
+class App {
+
+    fun run() {
+        var lastId = 0
+
+        println("== 명언 앱 ==")
+
+        while(true) {
+            print("명령) ")
+
+            val input = readln()
+            when (input) {
+                "종료" -> {
+                    println("프로그램을 종료합니다.")
+                    break
+                }
+                "등록" -> {
+                    print("명언: ")
+                    val content = readln()
+                    print("작가: ")
+                    val author = readln()
+                    val id = ++lastId
+
+                    println("${id}번 명언이 등록되었습니다.")
+                }
+            }
+        }
+    }
+}
