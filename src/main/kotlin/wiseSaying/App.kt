f3ffedf480938240a646cde1259cc0f239d4e9f9
sat7312 <sat7312@gmail.com>
2026-04-30 11:31:39 +0900

    5

--- src/main/kotlin/wiseSaying/App.kt
+++ src/main/kotlin/wiseSaying/App.kt
@@ -4,6 +4,7 @@ class App {
 
     fun run() {
         var lastId = 0
+        val wiseSayings = mutableListOf<WiseSaying>()
 
         println("== 명언 앱 ==")
 
@@ -23,7 +24,20 @@ class App {
                     val author = readln()
                     val id = ++lastId
 
+                    WiseSaying(lastId, content, author)
+                        .also {
+                            wiseSayings.add(it)
+                        }
+
                     println("${id}번 명언이 등록되었습니다.")
+                }
+
+                "목록" -> {
+                    println("번호 / 작가 / 명언")
+                    println("-".repeat(30))
+                    wiseSayings.reversed().forEach {
+                        println("${it.id} / ${it.author} / ${it.content}\n")
+                    }
                 }
             }
         }
